bdacd36d11a8566a86d3b2eef3d5faf751ea04cf
Robert Massaioli <robertmassaioli@gmail.com>
2011-12-13 01:20:32 +1100

    Added in some cool code.
    Unfortenately that code seems to be not working correctly on the actual
    device...

--- main.asm
+++ main.asm
@@ -14,15 +14,37 @@
 ; Hardware Connections
 ;  The only device that is connected to this PIC Micro is a LCD Screen because that is all that will fit. It only leaves one
 ;  output pin free. It would not be so bad if I got an LCD Screen that accepted 4-bit instead of 8-bit data lines. However
-;  that means that RA0-5 (6-bits) are connected to the LCD DB0-5 and RC0-1 are connected to LCD DB6-7. That leaves:
-;   - RC2 => LCD E
-;   - RC3 => LCD RW
+;  that means that RC0-5 (6-bits) are connected to the LCD DB0-5 and RA0-1 are connected to LCD DB6-7. That leaves:
+;   - RA2 => LCD E
 ;   - RC4 => LCD RS
+;   - RA5 => LCD RW
 ;  The only pin that leaves free on the 16f688 in RC5 which can be used as an LED status light maybe.
+
+; minus by one because it is zero based
+prog_len 		equ (4 - 1)
+timeout_high 	equ 0x04
+timeout_low 	equ 0x00
+
+; LCD Defines
+LCD_E 	set RA2
+LCD_RS 	set RA4
+LCD_RW 	set RA5
+
+LCD_DELAY_START equ 0xEB 	; 30ms
+LCD_DELAY_MED   equ 0x0C 	; 1.5ms
+
+LCD_CONTROL_DELAY 	equ 0x13
+LCD_WRITE_DELAY 	equ 0x15
+
+; LCD Control Commands
+LCD_CLEAR_SCREEN 	equ b'00000001'
+LCD_RETURN_HOME 	equ b'00000010'
 
  LIST P=16f688
  processor 16f688
  include <p16f688.inc>
+ 
+ extern copy_init_data
 
 ; The WDT needs to be off because we do not want it resetting on us.
 ; INTOSCIO means it will run at 8MHz
@@ -39,39 +61,98 @@
 	LPROG:8
 	LCOUNT:4 	; The Longest number of characters ever printed
 	TIMEOUT:2
+	DELAY
+	LCDOUT
+	LCDCONTROL
  endc
+ 
+;;;;;;
+;; Macros
+;;;;;;
+
+; keep in mind that macros need to be declared before they are used
+
+; increments the PC and wraps it at the 4-bit boundary too
+increment_pc macro
+ incf PC, 1
+ movlw 0x0F
+ andwf PC, 1
+ endm
+
+wrap_r0 macro
+ movlw 0x0F
+ andwf R0, 1
+ endm
+ 
+wrap_r1 macro
+ movlw 0x0F
+ andwf R1, 1
+ endm
+
+; loads the current value into w 
+lcw macro
+ movlw PROG
+ addwf PC, 0
+ movwf FSR
+ movf INDF, 0
+ endm
 
 ; The start of the program
- org 0
+ org 0x0
  goto main
-
-; minus by one because it is zero based
-prog_len 		equ (4 - 1)
-timeout_high 	equ 0x04
-timeout_low 	equ 0x00
-
-program:
- addwf PCL
- dt 0x1,0x8,0x8,0xF,0x6,0x8,0xD,0x2,0x5,0xF,0x0,0x0,0x0,0x0,0x0,0x0
+ 
+; Interrupts are handled here
+ org 0x4
+ banksel INTCON
+ btfss INTCON, T0IF
+  goto tmr0_interrupt_end
+  
+ movf DELAY, F
+ btfss STATUS, Z
+  decf DELAY, F
+ 
+ bcf INTCON, T0IF
+ 
+tmr0_interrupt_end:
+ banksel PC
+ retfie
 
 main:
-; In this case you load the program from the bottom and run it
-#if 0
- ; Load the entire program into ram
- movlw CPROG
- movwf FSR
- clrf  PC	; here the PC is used to wrap through the entire program
+ pagesel setup_hardware
+ call setup_hardware
+ banksel LCDCONTROL
+ bcf LCDCONTROL, 0
  
-read_in_memory:
- movfw PC
- call program
- movwf INDF
- incf FSR, 1
- incf PC, 1
- btfss PC, 4 	; when you reach PC = 16 then it is time to stop
-  goto read_in_memory
-#else
- ; In this case you set everything to zero and just run it
+ movlw 'R'
+ banksel LCDOUT
+ movwf LCDOUT
+ pagesel lcd_write_data_busy
+ call lcd_write_data_busy
+ 
+ movlw 'O'
+ banksel LCDOUT
+ movwf LCDOUT
+ pagesel lcd_write_data_busy
+ call lcd_write_data_busy
+ 
+ pagesel copy_init_data
+ call copy_init_data
+ 
+ banksel hex_st
+ movlw hex_st
+ movwf FSR
+ movlw 0xA
+ addwf FSR, F
+ bankisel hex_st
+ movf INDF, W
+ banksel LCDOUT
+ movwf LCDOUT
+ pagesel lcd_write_data_busy
+ call lcd_write_data_busy
+ goto $
+ 
+ call print_status
+
  ; set all of the program memory to zero
  clrf PC
  movlw CPROG
@@ -83,7 +164,6 @@ set_to_w_loop:
  incf PC, 1
  btfss PC, 4
   goto set_to_w_loop
-#endif
 
  ; clear LCOUNT
  movlw LCOUNT
@@ -132,8 +212,11 @@ inner_loop:
  subwf   (LCOUNT+3), W
  btfsc STATUS, C
   goto no_update
-  
+ 
+ ; It is better than the previous results, update 
  call results_to_lprog
+ ; Write the current best result to the screen
+ call print_status
  
 no_update:
  ; now add one to the program
@@ -143,7 +226,7 @@ no_update:
 prog_inc_loop:
  incf INDF, F
  movlw 0x0F
- andwf INDF
+ andwf INDF, F
  btfss STATUS, Z
   goto start_calc_loop
  incf FSR, F
@@ -159,12 +242,209 @@ end_calc_loop:
 
 fin: 
  ; now we are done, you are free to loop forever doing nothing
- movf LCOUNT, W
+ ;call print_status
  goto $
 
 ;;;;;;
 ;; Helper Functions
 ;;;;;;
+
+; setup_hardware - function
+; Purpose:
+;  This function sets up all of the hardware in the device.
+setup_hardware:
+ ; Set all pins to be output pins
+ ; WARNING: RA3 cannot be set to output, it will remain as an input pin...WTF right?
+ banksel PORTA
+ clrf PORTA
+ clrf PORTC
+ ; Digital I/O
+ movlw 0x7
+ movwf CMCON0
+ banksel ANSEL
+ clrf ANSEL
+ ; Set all ports as outputs
+ banksel TRISA
+ clrf TRISA
+ clrf TRISC
+ 
+ ; Start the TMR0
+ banksel OPTION_REG
+ bcf OPTION_REG, T0CS
+ 
+ ; Disable interrupts
+ banksel INTCON
+ bcf INTCON, GIE
+ 
+ ; setup the lcd screen
+ call lcd_setup
+ 
+ return
+ 
+; setup_lcd - function
+; Purpose:
+;  This function sets up the lcd to be used correctly.
+lcd_setup:
+ banksel DELAY
+ movlw LCD_DELAY_START
+ movwf DELAY
+ call lcd_delay
+ 
+ ; setup the lcd
+ bsf LCDCONTROL, 0  ; these are all control messages
+ 
+ ; Two Line mode with small font
+ movlw b'00111000'
+ movwf LCDOUT
+ call lcd_write_data
+ 
+ movlw LCD_CONTROL_DELAY
+ movwf DELAY 		
+ call lcd_short_delay
+ 
+ ; Display On, Cursor Off, Blink Off
+ movlw b'00001101'
+ movwf LCDOUT
+ call lcd_write_data
+ 
+ movlw LCD_CONTROL_DELAY
+ movwf DELAY
+ call lcd_short_delay
+ 
+ ; Clear the display
+ movlw LCD_CLEAR_SCREEN
+ movwf LCDOUT
+ call lcd_write_data
+ 
+ movlw LCD_DELAY_MED
+ movwf DELAY
+ call lcd_delay
+ 
+ ; set the entry mode to increment, no shift
+ movlw b'00000110'
+ movwf LCDOUT
+ call lcd_write_data
+ 
+ return
+ 
+lcd_wait_busy:
+ ; read the ports
+ banksel TRISA
+ movlw b'11'
+ iorwf TRISA, F
+ banksel TRISC
+ movlw 0xFF
+ movwf TRISC
+ 
+ ; now start reading the line
+ banksel PORTA
+ bcf PORTA, LCD_RS
+ bsf PORTA, LCD_RW
+ bsf PORTA, LCD_E
+ 
+ nop ; just in case nop
+ nop
+ nop
+ 
+ btfsc PORTA, RA1
+  goto $-1
+ 
+ bcf PORTA, LCD_E
+ return
+
+lcd_write_data_busy:
+ pagesel lcd_wait_busy
+ call lcd_wait_busy
+ pagesel lcd_write_data
+ call lcd_write_data
+ return
+
+; lcd_write_data - function
+; Purpose
+;  The purpose of this function is to write the data in LCDOUT to the LCD
+;  If the first bit of LCDCONTROL is 1 then it will be a control message and if not then it will be a write message
+lcd_write_data:
+ ; setup ports
+ banksel TRISA
+ clrf TRISA
+ banksel TRISC
+ clrf TRISC
+ 
+ ; setup the message
+ banksel LCDOUT
+ movf LCDOUT, W
+ movwf PORTC
+ 
+ bcf PORTA, RA0
+ btfsc LCDOUT, 6
+  bsf PORTA, RA0
+ 
+ bcf PORTA, RA1
+ btfsc LCDOUT, 7
+  bsf PORTA, RA1
+ 
+ bcf PORTA, LCD_RW
+ 
+ ; If it is a control message or not then set it correctly
+ btfss LCDCONTROL, 0
+  bsf PORTA, LCD_RS
+ btfsc LCDCONTROL, 0
+  bcf PORTA, LCD_RS
+  
+ nop
+ 
+ ; now enable the message send and delay the appropriate time
+ bsf PORTA, LCD_E
+ nop ; we only need a 230ns delay which is less than one instruction
+  ; now turn of the enable
+ bcf PORTA, LCD_E
+ bcf PORTA, LCD_RS
+ bcf PORTA, LCD_RW
+ 
+ ; wait for the device to recognise the message
+ nop
+ nop
+ 
+ return
+
+; lcd_short_delay - function
+; This has a short delay measured in instructions. It takes 2c to call into this function. 2c to leave this function.
+; It also uses: (delay - 1) * 4 + 3 in general operation.
+; Therefore, Total Delay: 7 + 4(delay - 1)
+lcd_short_delay:
+ decf DELAY, F			; 1c
+ btfss STATUS, Z		; 2c if skip and, 1c if not
+  goto lcd_short_delay  ; 2c every time
+  
+ return					; 2c every time
+
+; lcd_delay - This function expects to wait a set amount of time before being delayed.
+; You just set the DELAY registers to the right values and it just works.
+lcd_delay:
+ ; turn on TMR0 Interrupts
+ banksel INTCON
+ bcf INTCON, T0IF
+ bsf INTCON, T0IE
+ bsf INTCON, GIE
+ 
+ ; clear the timer
+ banksel TMR0
+ clrf TMR0
+ 
+ ; poll until the delay variable is 0
+ banksel DELAY
+lcd_delay_poll:
+ nop
+ nop
+ movf DELAY, F
+ btfss STATUS, Z
+ goto lcd_delay_poll
+ 
+ ; turn off TMR0 Interrupts
+ banksel INTCON
+ bcf INTCON, GIE
+ bcf INTCON, T0IE
+ return
 
 ; current_to_prog - function
 ; input values:
@@ -205,6 +485,80 @@ ctp_loop:
  
  return
  
+; print_status - function
+; Purpose:
+;  The purpose of this function is to print out the current status of the device to the LCD screen.
+;  Now the screen has two rows, each with 16 characters printable. For that purpose:
+;   - The top row will show the current largest program in hexidecimal
+;   - The bottom row will show "Len: <num> [Done]"
+print_status:
+ bsf LCDCONTROL, 0
+ movlw LCD_CLEAR_SCREEN
+ movfw LCDOUT
+ pagesel lcd_write_data_busy
+ call lcd_write_data_busy
+
+ banksel FSR
+ movlw LPROG
+ movwf FSR
+ bcf LCDCONTROL, 0
+
+ movlw D'8'
+ movwf PC 	; Use PC as the counter down
+print_status_lprog:
+ movf INDF, W
+ movwf TEMP
+ pagesel print_byte_as_hex
+ call print_byte_as_hex
+ decf PC, F
+ btfss STATUS, Z
+  goto print_status_lprog
+ 
+ return
+
+; print_byte_as_hex - function
+; This function assumes that the number to print is in TEMP
+; This function assumes that the lcd is in write mode not control mode
+print_byte_as_hex:
+ swapf TEMP, W
+ andlw 0x0F
+ pagesel get_hex_char
+ call get_hex_char
+ banksel LCDOUT
+ movwf LCDOUT
+ pagesel lcd_write_data_busy
+ call lcd_write_data_busy
+ 
+ movf TEMP, W
+ andlw 0x0F
+ pagesel get_hex_char
+ call get_hex_char
+ banksel LCDOUT
+ movwf LCDOUT
+ pagesel lcd_write_data_busy
+ call lcd_write_data_busy
+ return
+
+; get_hex_char - function
+; Assumes that W contains the value to convert to hex
+get_hex_char:
+ ; select the character to read
+ banksel FSR
+ movwf FSR
+ movlw hex_st
+ addwf FSR, F
+ bankisel hex_st
+ movf INDF, W
+ return
+
+; print_string - function
+; Assumes that WREG holds the string that you want to print
+print_string:
+ return
+
+print_number:
+ return
+ 
 ; results_to_lprog - function
 ; Purpose:
 ;  The purpose of this function is to copy the values in the current program to the largest program save registers.
@@ -223,10 +577,7 @@ rtl_loop:
  incf R0, F
  movf INDF, W
  movwf TEMP
- rlf TEMP, F
- rlf TEMP, F
- rlf TEMP, F
- rlf TEMP, F
+ swapf TEMP, F  ; Put it on the other side and make way for the next.
  incf FSR, F
  incf R0, F
  movf INDF, W
@@ -315,6 +666,7 @@ run_program:
  movwf FSR
  movlw 0x4
  movwf TEMP
+ pagesel clear_multibyte
  call clear_multibyte
  
  ; clear TIMEOUT
@@ -322,6 +674,7 @@ run_program:
  movwf FSR
  movlw 0x2
  movwf TEMP
+ pagesel clear_multibyte
  call clear_multibyte
 
  movlw LCOUNT
@@ -348,6 +701,7 @@ rp_main_loop:
  movwf FSR
  movlw 0x4
  movwf TEMP
+ pagesel clear_multibyte
  call clear_multibyte
  goto rp_fin
 ; load the next instruction
@@ -355,7 +709,13 @@ continue_main_loop:
  lcw
 
 ; add it to the PCL so that you use the jump table
- addwf PCL, 1
+ movwf TEMP
+ pageselw rp_begin_jumptable
+ movf TEMP, W
+ addlw rp_begin_jumptable
+ btfsc STATUS, C
+  incf PCLATH, F
+ movwf PCL
  
 rp_begin_jumptable:
  goto rp_zero
@@ -576,33 +936,10 @@ clear_multibyte:
   goto clear_multibyte
  return
 
-;;;;;;
-;; Macros
-;;;;;;
-
-; increments the PC and wraps it at the 4-bit boundary too
-increment_pc macro
- incf PC, 1
- movlw 0x0F
- andwf PC, 1
- endm
-
-wrap_r0 macro
- movlw 0x0F
- andwf R0, 1
- endm
- 
-wrap_r1 macro
- movlw 0x0F
- andwf R1, 1
- endm
-
-; loads the current value into w 
-lcw macro
- movlw PROG
- addwf PC, 0
- movwf FSR
- movf INDF, 0
- endm
+string_data idata 0xA0
+welcome_st 	db "4917 Solver", 0
+len_st 		db "Len: ", 0
+done_st 	db "Done", 0
+hex_st 		db "0123456789ABCDEF"
 
  end
